a2b44e16da49d604d1c914d0f593d939d6df18e6
musabalki <musabalkimb@gmail.com>
2023-02-22 23:58:04 +0300

    Add examples

---
+++ second/pages/product.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import React from 'react'
+
+export default function product() {
+  return (
+    <div>
+    <Head>
+        <title>PRODUCT</title>
+    </Head>
+    product
+    </div>
+  )
+}
