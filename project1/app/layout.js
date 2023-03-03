e17b84185783239d01bcee7db9216481048919d7
musabalki <musabalkimb@gmail.com>
2023-03-04 00:04:17 +0300

    Add layout, page

--- project1/app/layout.js
+++ project1/app/layout.js
@@ -1,7 +1,13 @@
+import { Inter } from "next/font/google";
+import "@/styles/reset.css"
+import "@/styles/globals.css"
+
+const interFontFamily = Inter({subsets:['latin']})
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="tr" className={interFontFamily.className}>
+      <body className="container">{children}
+      </body>
     </html>
   );
 }
