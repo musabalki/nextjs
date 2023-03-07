fd9189b10d982b44d7a23918efd57a9bdb9498ae
musabalki <musabalkimb@gmail.com>
2023-03-07 21:46:56 +0300

    Add csr

--- rendering/app/page.jsx
+++ rendering/app/page.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+'use client'
+import React, { useEffect,useState } from 'react'
 
 export default function Page() {
+  const [message,setMessage] = useState("initial");
+  useEffect(()=>{
+    setMessage("useEffect Message")
+  },[])
   console.log("Page")
   return (
-    <div>Page</div>
+    <div>Page
+      {message}
+    </div>
   )
 }
