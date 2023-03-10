fa08b1b6b84b964a81e6ff59c9c12f357b2f7d20
musabalki <musabalkimb@gmail.com>
2023-03-10 23:49:32 +0300

    Add data fetching

--- rendering/app/page.jsx
+++ rendering/app/page.jsx
@@ -1,15 +1,18 @@
 'use client'
 import React, { useEffect,useState } from 'react'
 
-export default function Page() {
-  const [message,setMessage] = useState("initial");
+const getData = async() =>{
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/1")
+  return  res.json();
+}
+
+export default async function Page() {
+  const data = await getData();
+  console.log(data)
+  /*const [message,setMessage] = useState("initial");
   useEffect(()=>{
     setMessage("useEffect Message")
   },[])
-  console.log("Page")
-  return (
-    <div>Page
-      {message}
-    </div>
-  )
+  console.log("Page")*/
+  return (<div>Pagetest</div>)
 }
