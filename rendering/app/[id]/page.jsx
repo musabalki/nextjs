e58a796707512f1250f93fe574b6e62a1ba86a51
musabalki <musabalkimb@gmail.com>
2023-03-12 00:29:14 +0300

    Add data fetching, generateStaticParams

--- rendering/app/[id]/page.jsx
+++ rendering/app/[id]/page.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
-export default function IdPage({params}) {
-    console.log("Idpage",params.id)
+export const getAllPosts = async (id) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+  return res.json()
+}
+
+export const getSinglePost = async (id) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+  return res.json()
+}
+
+export default async function IdPage({params}) {
+  //console.log("Idpage",params.id)
+  const data = await getSinglePost(params.id)
+
   return (
-    <div>IdPage {params.id} </div>
+    <div>IdPage {data.title} </div>
   )
+}
+
+export async function generateStaticParams(){
+  const posts  = await getAllPosts()
+  
+  return posts.map(post=>{id:`${post.id}`})
 }
--- rendering/app/page.jsx
+++ rendering/app/page.jsx
@@ -2,18 +2,24 @@
 import React, { useEffect,useState } from 'react'
 
 const getData = async() =>{
-  /*const res = await fetch("https://jsonplaceholder.typicode.com/posts/1",{
-    cache:"no-store"
-  })*/
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts/1",{
-    next:{revalidate:10}
-  })
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/1")
   return  res.json();
 }
-
+const getData2 = async() =>{
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/2")
+  return  res.json();
+}
+const getData3 = async() =>{
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/3")
+  return  res.json();
+}
 export default async function Page() {
-  const data = await getData();
-  console.log(data)
+  const data =  getData();
+  const data2 =  getData2();
+  const data3 =  getData3();
+  const dataResult = await Promise.all([data,data2,data3])
+
+  console.log(dataResult)
   /*const [message,setMessage] = useState("initial");
   useEffect(()=>{
     setMessage("useEffect Message")
